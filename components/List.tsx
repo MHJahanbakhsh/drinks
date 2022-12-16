a94334a66b0828224ab8c117c52c453fe02365ab
MohammadHossein Jahanbakhsh <mammadjahanbakhsh@yahoo.com>
2022-12-16 18:42:30 +0330

    fix bug of reset button

--- components/List.tsx
+++ components/List.tsx
@@ -7,54 +7,78 @@ import { DrinksContext } from "../pages";
 
 //mui
 import Grid from "@mui/material/Grid";
-import SortIcon from "@mui/icons-material/Sort"
-import Button from '@mui/material/Button';
-
+import SortIcon from "@mui/icons-material/Sort";
+import Button from "@mui/material/Button";
+import { Typography } from "@mui/material";
 
 //styles
 import styles from "../styles/List.module.css";
 
-//@types
-// import { IDrink } from "../@types/drink";
+
 
 const List = () => {
-  const {arrOfDrinks,setArrOfDrinks} = useContext(DrinksContext);
-  const drinks = React.useRef(arrOfDrinks)
-  const [sorted, setSorted] = React.useState(false)
+  const { arrOfDrinks, setArrOfDrinks } = useContext(DrinksContext);
+  const drinksRef = React.useRef(arrOfDrinks);
+  const [sorted, setSorted] = React.useState(false);
+  React.useEffect(()=>{
+    setSorted(false)
+    drinksRef.current = arrOfDrinks
+  },[arrOfDrinks.length])
 
   const onSortHandler = () => {
-    if(sorted) return
-    console.log('sorting')
+    if (sorted) return;
+    console.log("sorting");
     let newArray = arrOfDrinks.map((e) => e);
     setArrOfDrinks(newArray.sort((a, b) => (a.strDrink > b.strDrink ? 1 : -1)));
-    setSorted(true)
+    setSorted(true);
   };
 
-  const onResetHandler = ()=>{
-    console.log('resetting')
-    setArrOfDrinks(drinks.current)
-    setSorted(false)
-  }
+  const onResetHandler = () => {
+    console.log("resetting");
+    console.log({arrOfDrinks})
+    
+    setArrOfDrinks(drinksRef.current)
+    
+    setSorted(false);
+  };
 
   return (
-    <>
-      <div className={styles.iconsContainer} >
-        <Button disabled={!sorted} className={styles.icon} variant="outlined" onClick={onResetHandler}>Reset</Button>
-        <SortIcon  className={styles.icon} onClick={onSortHandler}/>
+    <div  style={{ minHeight: "65vh" }}>
+      {arrOfDrinks.length === 0 ? 
+          <Typography
+            variant="h4"
+            align="center"
+          >
+            No matching result with your search
+          </Typography>
+         :<>
+      <div className={styles.iconsContainer}>
+        <Button
+          disabled={!sorted}
+          className={styles.icon}
+          variant="outlined"
+          onClick={onResetHandler}
+        >
+          Reset
+        </Button>
+        <SortIcon className={styles.icon} onClick={onSortHandler} />
       </div>
       <div>
-        <Grid container spacing={4}>
-          {arrOfDrinks.map((drink) => {
-            return (
-              <Grid key={drink.idDrink} item xs={12} sm={6} md={4}>
-                <Item {...drink} />
-              </Grid>
-            );
-          })}
-        </Grid>
+         
+          <Grid container spacing={4}>
+            {arrOfDrinks.map((drink) => {
+              return (
+                <Grid key={drink.idDrink} item xs={12} sm={6} md={4}>
+                  <Item {...drink} />
+                </Grid>
+              );
+            })}
+          </Grid>
+        
       </div>
-    </>
+      </>}
+    </div>
   );
 };
 
-export default List;
+export default React.memo(List);
